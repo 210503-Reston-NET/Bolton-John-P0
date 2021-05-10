99a7d43810dd80ccb9b77513b4c5b83c947d9453
johncbolton <johncollierbolton@gmail.com>
2021-05-10 11:42:31 -0600

    Product file
    This is the product info

---
+++ APP/SModels/Products.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace SMdodels
+{
+///<summary>
+///This structure has the product data type.
+///</summary>
+public class Product
+{
+  public string Name {get;set;}
+  public string Description {get;set;}
+  public double Price {get; set;}
+  pubblic int IDNumber {get;set;}
+  
+  public Prodcut(string name, string description, double price, int idnumber)
+  {
+    this.Name= name;
+    this.Description =description;
+    this.Price=price;
+    this.IDNumber=idnumber;
+ }
+ 
+ public Product(){
+ 
+ public override string ToString()
+ {
+ return$:"Name:{this.Name}\n Description: {this.Description}\n Price: {this.Price} \IDNumber: {this.IDNumber}";
+ }
+ 
+}
+}
